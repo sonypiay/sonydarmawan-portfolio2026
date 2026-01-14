b9686edd3ea53e4ca1df352dc17df3e1a7366188
sonypiay <pyscho30@gmail.com>
2026-01-15 02:01:59 +0700

    remove column email_verified_at on table admin

--- database/migrations/2026_01_15_010927_create_admin_table.php
+++ database/migrations/2026_01_15_010927_create_admin_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->char('id', 36)->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
         });
