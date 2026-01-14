6ca408ebe2dfa569024eec74d2e83708b0869be3
sonypiay <pyscho30@gmail.com>
2026-01-15 01:37:49 +0700

    initiate commit + migration table

---
+++ database/migrations/2026_01_14_180739_create_portfolio_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolio', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('headline_image');
+            $table->timestamps();
+        });
+
+        Schema::create('portfolio_images', function (Blueprint $table) {
+            $table->id('id');
+            $table->unsignedBigInteger('portfolio_id');
+            $table->string('image_path');
+            $table->timestamps();
+
+            $table->foreign('portfolio_id')->references('id')->on('portfolio')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolio_images');
+        Schema::dropIfExists('portfolio');
+    }
+};
