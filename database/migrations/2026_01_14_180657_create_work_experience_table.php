78956ae4149980272c95776cbbdf32d905919b3b
sonypiay <pyscho30@gmail.com>
2026-01-15 02:05:56 +0700

    change table name work experience

--- database/migrations/2026_01_14_180657_create_work_experience_table.php
+++ database/migrations/2026_01_14_180657_create_work_experience_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('works_experience', function (Blueprint $table) {
+        Schema::create('work_experience', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
             $table->string('position', 100);
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('works_experience');
+        Schema::dropIfExists('work_experience');
     }
 };
